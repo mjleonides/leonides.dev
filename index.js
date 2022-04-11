71a3894e9c78d180cff97d3147f79d795961583d
mjleonides <101823799+mjleonides@users.noreply.github.com>
2022-04-11 17:38:07 -0500

    Version 2!

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-const menuToggle = document.querySelector(".menu-toggle");
-const menu = document.querySelector(".menu");
-const nav = document.getElementById("nav");
+const menuToggle = document.getElementById('menu-toggle');
+const themeToggle = document.getElementById('theme-toggle');
+const r = document.querySelector(':root');
+const setTheme = localStorage.getItem('theme');
+
+if (setTheme === 'dark') {
+  r.style.setProperty('--theme-background', 'black');
+  r.style.setProperty('--theme-color', 'white');
+}
 
 menuToggle.addEventListener('click', function() {
-    nav.classList.toggle("active-nav");
-    menu.classList.toggle("active-menu");
+  document.querySelector(".menu").classList.toggle('active-menu');
+  document.querySelector("#menu-toggle").classList.toggle('active-toggle');
 });
 
+themeToggle.addEventListener('click', function() {
+  const themeBackgroundColor = getComputedStyle(r).getPropertyValue('--theme-background');
+  const themeColor = getComputedStyle(r).getPropertyValue('--theme-color');
+  if (themeBackgroundColor === 'white') {
+    r.style.setProperty('--theme-background', 'black');
+    r.style.setProperty('--theme-color', 'white');
+    localStorage.setItem('theme', 'dark');
+  } else {
+    r.style.setProperty('--theme-background', 'white');
+    r.style.setProperty('--theme-color', 'black');
+    localStorage.setItem('theme', 'light');
+  }})
